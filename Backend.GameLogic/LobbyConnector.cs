29ae33412094332ec3e307de6161941967f30dad
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2014-03-24 16:12:32 +0100

    Empty cloud project added

---
+++ Backend.GameLogic/LobbyConnector.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.GameLogic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class LobbyConnector
+    {
+
+    }
+}
