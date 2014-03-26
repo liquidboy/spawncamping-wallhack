1471eee5063a63829c0951f1319485def2e5eebe
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2014-03-26 21:24:46 +0100

    Service Bus is now observable

---
+++ Backend.Utils/Networking/Extensions/ObservableServiceBusExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Backend.Utils.Networking.Extensions
+{
+    using System;
+    using System.Reactive.Linq;
+    using Microsoft.ServiceBus.Messaging;
+
+    public static class ObservableServiceBusExtensions
+    {
+        public static IObservable<BrokeredMessage> CreateObervable(this SubscriptionClient client)
+        {
+            return Observable.Create<BrokeredMessage>(async (observer) =>
+            {
+                try
+                {
+                    while (!client.IsClosed)
+                    {
+                        var message = await client.ReceiveAsync();
+                        if (message == null)
+                        {
+                            observer.OnCompleted();
+                            break;
+                        }
+
+                        observer.OnNext(message);
+                    }
+                }
+                catch (Exception e)
+                {
+                    observer.OnError(e);
+                }
+            });
+        }
+    }
+}
