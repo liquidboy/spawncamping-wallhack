0c1eae06067d2a77e12376bde6309117ade38dba
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2014-03-26 08:19:11 +0100

    a

--- Backend.Utils/Networking/Extensions/SocketExtensions.cs
+++ Backend.Utils/Networking/Extensions/SocketExtensions.cs
@@ -48,33 +48,9 @@
 
         #endregion
 
-        private static readonly Encoding DEFAULT_ENCODING = Encoding.UTF8;
-
-        public static async Task<Int32> ReadInt32Async(this Socket socket)
-        {
-            return await socket.ReadValueAsync<Int32>(sizeof(Int32), _ => BitConverter.ToInt32(_, 0));
-        }
-        public static async Task<byte> ReadByteAsync(this Socket socket)
-        {
-            return await socket.ReadValueAsync<byte>(sizeof(byte), _ => _[0]);
-        }
-
-        public static async Task<T> ReadValueAsync<T>(this Socket socket, int length, Func<byte[], Task<T>> convertAsync)
-        {
-            byte[] buf = new byte[length];
-            var bytesRead = await socket.ReadAsync(buf, 0, buf.Length);
-            if (bytesRead != buf.Length) throw new Exception("Missing bytes");
-            return await convertAsync(buf);
-        }
-        public static async Task<T> ReadValueAsync<T>(this Socket socket, int length, Func<byte[], T> convertAsync)
-        {
-            byte[] buf = new byte[length];
-            var bytesRead = await socket.ReadAsync(buf, 0, buf.Length);
-            if (bytesRead != buf.Length) throw new Exception("Missing bytes");
-            return convertAsync(buf);
-        }
-
         #region string
+
+        private static readonly Encoding DEFAULT_ENCODING = Encoding.UTF8;
 
         public static async Task<string> ReadStringAsync(this Socket socket)
         {
@@ -91,10 +67,48 @@
             var bytesWritten = await socket.WriteAsync(bytes, 0, bytes.Length);
         }
 
+        #endregion
+
+        #region Int32
+
+        public static async Task<Int32> ReadInt32Async(this Socket socket)
+        {
+            return await socket.ReadValueAsync<Int32>(sizeof(Int32), _ => BitConverter.ToInt32(_, 0));
+        }
+
         public static async Task WriteAsync(this Socket socket, Int32 value)
         {
             var buf = BitConverter.GetBytes(value);
             await socket.WriteAsync(buf, 0, sizeof(Int32));
+        }
+
+        #endregion
+
+        #region byte
+
+        public static async Task<byte> ReadByteAsync(this Socket socket)
+        {
+            return await socket.ReadValueAsync<byte>(sizeof(byte), _ => _[0]);
+        }
+
+        public static async Task WriteAsync(this Socket socket, byte value)
+        {
+            await socket.WriteAsync(new byte[1]{value} , 0, 1);
+        }
+
+        #endregion
+
+        #region utils
+        private static async Task<T> ReadValueAsync<T>(this Socket socket, int length, Func<byte[], T> convertAsync)
+        {
+            return await ReadValueAsync(socket, length, async (_) => convertAsync(_));
+        }
+        private static async Task<T> ReadValueAsync<T>(this Socket socket, int length, Func<byte[], Task<T>> convertAsync)
+        {
+            byte[] buf = new byte[length];
+            var bytesRead = await socket.ReadAsync(buf, 0, buf.Length);
+            if (bytesRead != buf.Length) throw new Exception("Missing bytes");
+            return await convertAsync(buf);
         }
 
         #endregion
